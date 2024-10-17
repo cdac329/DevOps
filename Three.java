521308656a77ec4f426e4a6ef212914925c40122
ActsAtharva <actsatharva@gmail.com>
2024-10-17 14:08:16 +0530

    Create Three.java

---
+++ Three.java
@@ -0,0 +1,17 @@
+public class Three{
+public static void main(String[] args) {
+int a =5;
+int b=4;
+int c=2;
+if(a>b && a>c){
+    System.out.println("this number is greater"+a);
+}
+    if(b>a && b>c){
+        System.out.println("this number is greater"+b);
+    }
+    else{
+        System.out.println("this number is greater"+c);
+    }
+}
+
+}
